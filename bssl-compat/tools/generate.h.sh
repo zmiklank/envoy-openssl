aed9775e34d99fbe81063f214abab6a5836b60e7
Zuzana Miklankova <zmiklank@redhat.com>
2024-09-30 11:33:01 +0200

    finish bssl headers gen

--- bssl-compat/tools/generate.h.sh
+++ bssl-compat/tools/generate.h.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
 set -e # Quit on error
-set -x # Echo commands
-
-#function status {
-#    cmake -E cmake_echo_color --blue "$1"
-#}
-#
-#function warn {
-#    cmake -E cmake_echo_color --yellow "$1"
-#}
-#
-#function error {
-#    cmake -E cmake_echo_color --red "$1"
-#    exit 1
-#}
-
+# set -x # Echo commands
 
 #
 # Get command line args
@@ -23,18 +9,16 @@ set -x # Echo commands
 UNCOMMENT_SCRIPT="${1?"UNCOMMENT_SCRIPT not specified"}"
 SRC_FILE="${2?"SRC_FILE not specified"}" # e.g. crypto/err/internal.h
 DST_FILE="${3?"DST_FILE not specified"}" # e.g. source/crypto/err/internal.h
+DST_DIR="${4?"DST_DIR not specified"}"
 
 PATCH_DIR="./bssl-compat/patch"
-pwd
-ls bssl-compat
 
 #
 # Check/Ensure the inputs and outputs exist
 #
 [ -d "${PATCH_DIR}" ] || error "PATCH_DIR $PATCH_DIR does not exist"
 [ -f "${UNCOMMENT_SCRIPT}" ] || error "UNCOMMENT_SCRIPT $UNCOMMENT_SCRIPT does not exist"
-[ -f "${SRC_FILR}" ] || error "SRC_FILE $SRC_FILE does not exist"
-[ -f "${DST_FILE}" ] || error "DST_FILE $DST_FILE does not exist"
+[ -f "${SRC_FILE}" ] || error "SRC_FILE $SRC_FILE does not exist"
 mkdir -p "$(dirname "$DST_FILE")"
 
 
@@ -48,7 +32,6 @@ cp "$SRC_FILE" "$GEN_APPLIED_SCRIPT"
 if [ -f "$PATCH_SCRIPT" ]; then
     PATH="$(dirname "$0"):$PATH" "$PATCH_SCRIPT" "$GEN_APPLIED_SCRIPT"
 else # Comment out the whole file contents
-    echo "printing"
     $UNCOMMENT_SCRIPT "$GEN_APPLIED_SCRIPT" --comment
 fi
 
@@ -70,4 +53,4 @@ fi
 # Copy result to the destination
 # ==============================
 #
-cp "$GEN_APPLIED_PATCH" "$DST_FILE"
+cp "$GEN_APPLIED_PATCH" "$DST_DIR/$DST_FILE"
